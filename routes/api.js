fc6366802650c65fe168fb5640781ecd7724fec3
Glitch (steady-tracker) <none>
2019-01-09 14:32:05 +0000

    🎹🐊 Checkpoint ./routes/api.js:2505047/146

--- routes/api.js
+++ routes/api.js
@@ -58,16 +58,16 @@ module.exports = function (app) {
                 .limit(10)
                 .toArray(function(err, docs) {
                   if(err) console.log(err);
-                  let replies=[];
                   const result = docs.map(doc => {
                     //console.log(doc.replies);
                     let count = doc.replies.length;
                     if(count > 0){
                       if(count > 3) count = 3;
                       for(let i=0;i<count;i++){
-                         replies.push({_id: doc.replies.id, text: doc.replies.text, created_on: doc.replies.created_on})
+                         doc.replies[i] = {_id: doc.replies[i]._id, text: doc.replies[i].text, created_on: doc.replies[i].created_on};
                       }
                     }
+                    return doc;
                   });
                            
                   console.log(result);
