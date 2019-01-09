10932eba716493261623b77cd859f35b8b3d4695
Glitch (steady-tracker) <none>
2019-01-09 11:59:07 +0000

    🚦🐦 Checkpoint ./server.js:2505047/237 ./routes/api.js:2505047/955

--- routes/api.js
+++ routes/api.js
@@ -11,8 +11,24 @@
 var expect = require('chai').expect;
 
 module.exports = function (app) {
+  // I can POST a thread to a specific message board by passing form data text and 
+  // delete_password to /api/threads/{board}.(Recomend res.redirect to board page /b/{board}) 
+  // Saved will be _id, text, created_on(date&time), bumped_on(date&time, starts same as created_on), 
+  // reported(boolean), delete_password, & replies(array).
   
-  app.route('/api/threads/:board');
+  app.route('/api/threads/:board')
+    .post(function (req, res){
+       const board = req.body.board;
+       const text = req.body.text;
+       const delete_password = req.body.delete_password;
+       const cre
+    
+    console.log('board ', board);
+    console.log('text ', text);
+    console.log('delete_password ', delete_password);
+    
+  })
+    
     
   app.route('/api/replies/:board');
 
--- server.js
+++ server.js
@@ -20,8 +20,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //security
-app.use(helmet.frameguard({ action: 'deny' }));
+app.use(helmet.frameguard({ action: 'sameorigin' }));
 app.use(helmet.dnsPrefetchControl());
+app.use(helmet.referrerPolicy({ policy: 'same-origin' }));
 
 //Sample front-end
 app.route('/b/:board/')
